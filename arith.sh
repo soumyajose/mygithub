5ecdd5ae74839914aa56455c74b46078e544ae49
Soumya <soumyajose53@gmail.com>
2017-03-08 12:23:58 +0530

    committed

---
+++ arith.sh
@@ -0,0 +1,8 @@
+echo "enter two numbers"
+read a
+read b
+echo ` expr $a + $b `
+echo ` expr $a - $b `
+echo ` expr $a \* $b `
+echo ` expr $a / $b `
+
---
+++ swapping.sh
@@ -0,0 +1,9 @@
+echo "enter values for x"
+read x
+echo "enter values for y"
+read y
+echo "Before swap x=$x and y=$y"
+z=$x
+x=$y
+y=$z
+echo "After swapping x=$x and y=$y"
